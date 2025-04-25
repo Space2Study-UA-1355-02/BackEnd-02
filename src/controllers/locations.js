4c5fae0d3f8b04b6f8b99426848db36ab5d7ff83
ruslansymonenko <symonenkors@gmail.com>
2025-04-25 17:50:50 +0300

    added possibility to search countries and cities by entering first letters

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,15 +1,15 @@
 const locationsService = require('~/services/locations')
 
 const getCountries = async (req, res) => {
-  const { limit, offset } = req.query
-  const countries = await locationsService.getCountries(limit, offset)
+  const { limit, offset, search } = req.query
+  const countries = await locationsService.getCountries(limit, offset, search)
 
   res.status(200).json(countries)
 }
 
 const getCitiesByCountry = async (req, res) => {
-  const { country, limit, offset } = req.query
-  const countries = await locationsService.getCitiesByCountry(country, limit, offset)
+  const { country, limit, offset, search } = req.query
+  const countries = await locationsService.getCitiesByCountry(country, limit, offset, search)
 
   res.status(200).json(countries)
 }
--- src/services/locations.js
+++ src/services/locations.js
@@ -1,15 +1,18 @@
 const { getCountriesData, getCitiesDataByCountry } = require('~/utils/getLocationsApiData')
 const { paginate } = require('~/utils/paginate')
+const { filterBySearch } = require('~/utils/filterBySearch')
 
 const locationsService = {
-  getCountries: async (limit, offset) => {
+  getCountries: async (limit, offset, search) => {
     const paginationLimit = limit ? parseInt(limit) : 10
     const paginationOffset = offset ? parseInt(offset) : 0
 
     const countries = await getCountriesData()
+    const filteredCountries = filterBySearch(countries, search)
+
     return {
       data: {
-        countries: paginate(countries, paginationLimit, paginationOffset),
+        countries: paginate(filteredCountries, paginationLimit, paginationOffset),
         total: countries.length,
         limit: limit || countries.length,
         offset: offset || 0
@@ -17,15 +20,17 @@ const locationsService = {
     }
   },
 
-  getCitiesByCountry: async (countryName, limit, offset) => {
-    const cities = await getCitiesDataByCountry(countryName)
+  getCitiesByCountry: async (countryName, limit, offset, search) => {
     const paginationLimit = limit ? parseInt(limit) : 10
     const paginationOffset = offset ? parseInt(offset) : 0
+
+    const cities = await getCitiesDataByCountry(countryName)
+    const filteredCities = filterBySearch(cities, search)
 
     return {
       data: {
         countries: [countryName],
-        cities: paginate(cities, paginationLimit, paginationOffset),
+        cities: paginate(filteredCities, paginationLimit, paginationOffset),
         total: cities.length,
         limit: limit || cities.length,
         offset: offset || 0
