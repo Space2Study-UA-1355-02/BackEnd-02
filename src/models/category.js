d815ce3ff587a744cb1f0211c8e0987803cbb490
Roman Kryzhanivskiy <r.krijaniv@gmail.com>
2025-05-01 17:17:33 +0300

    fix validation incompatibility error

--- src/models/category.js
+++ src/models/category.js
@@ -8,16 +8,12 @@ const {
   FIELD_CANNOT_BE_LONGER
 } = require('~/consts/errors')
 const { CATEGORY } = require('~/consts/models')
-const {
-  regex: { HEX_COLOR_CODE_PATTERN, TITLE_PATTERN }
-} = require('~/consts/validation')
 
 const categorySchema = new Schema({
   name: {
     type: String,
     required: [true, FIELD_CANNOT_BE_EMPTY('name')],
     unique: [true, DOCUMENT_ALREADY_EXISTS('name')],
-    regex: TITLE_PATTERN,
     minLength: [1, FIELD_CANNOT_BE_SHORTER('name', 1)],
     maxLength: [100, FIELD_CANNOT_BE_LONGER('name', 100)]
   },
@@ -30,7 +26,6 @@ const categorySchema = new Schema({
     color: {
       type: String,
       required: [true, FIELD_CANNOT_BE_EMPTY('color')],
-      regex: HEX_COLOR_CODE_PATTERN,
       default: DEFAULT_CATEGORY_ICON_COLOR
     }
   }
