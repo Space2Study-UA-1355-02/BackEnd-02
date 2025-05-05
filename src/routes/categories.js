48019fb1987af5effa12d9969f45bfe2bbabc528
Roman Kryzhanivsky <77616134+Kryzhanivsky@users.noreply.github.com>
2025-05-05 20:57:22 +0300

    Closed (#98): Implement "Get Categories" endpoint (#17)
    * add swagger documentation
    
    * #98: Implement 'Get Categories' endpoint

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -10,6 +10,7 @@ const {
 } = require('~/consts/auth')
 
 router.use(authMiddleware)
+router.get('/', asyncWrapper(categoryController.getCategories))
 
 router.use(restrictTo(ADMIN))
 router.post('/', asyncWrapper(categoryController.createCategory))
