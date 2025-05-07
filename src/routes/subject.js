86c8c75203a4bb49cbc84e7653f42cf03144c6ab
Ruslan Symonenko <99801898+ruslansymonenko@users.noreply.github.com>
2025-05-07 13:35:34 +0300

    created main subject endpoints (#19)
    * created main subject endpoints
    
    * updated swagger documentation
    
    * fixed mistake in subject controller

--- src/routes/subject.js
+++ src/routes/subject.js
@@ -6,11 +6,22 @@ const isEntityValid = require('~/middlewares/entityValidation')
 
 const subjectController = require('~/controllers/subject')
 const Subject = require('~/models/subject')
+const Category = require('~/models/category')
+
+const body = [{ model: Category, idName: 'categoryId' }]
 
 const params = [{ model: Subject, idName: 'id' }]
 
 router.param('id', idValidation)
 
 router.get('/:id', isEntityValid({ params }), asyncWrapper(subjectController.getSubjectById))
+
+router.get('/', asyncWrapper(subjectController.getSubjects))
+
+router.post('/', isEntityValid({ body }), asyncWrapper(subjectController.createSubject))
+
+router.patch('/:id', isEntityValid({ params }), asyncWrapper(subjectController.updateOffer))
+
+router.delete('/:id', isEntityValid({ params }), asyncWrapper(subjectController.deleteSubject))
 
 module.exports = router
